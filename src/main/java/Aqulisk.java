7722b8e20379330d42268c8f68ea8298f3205a98
Володя Никоноров <dantes389@list.ru>
2023-03-19 15:00:17 +0300

    Start a game and crash due to error. Fix needed.

--- src/main/java/Aqulisk.java
+++ src/main/java/Aqulisk.java
@@ -1,33 +1,48 @@
 import bwapi.*;
-import bwta.BWTA;
 
-class Aqulisk extends DefaultBWListener {
-    private BWClient bwClient;
 
-    public Player self;
+public class Aqulisk extends DefaultBWListener {
+    BWClient bwClient;
+    Game game;
 
     @Override
     public void onStart() {
-        Game game = bwClient.getGame();
+        game = bwClient.getGame();
         game.setLocalSpeed(10);
-        BWTA.readMap(game);
-        BWTA.analyze();
-
     }
 
     @Override
     public void onFrame() {
-        Game game = bwClient.getGame();
-        game.drawTextScreen(10, 10, "Hello World!");
-        game.drawTextScreen(10, 20,
-                "Resources: " +
-                        self.minerals() + " minerals, " +
-                        self.gas() + " gas");
+        Player self = game.self();
+        game.drawTextScreen(20, 20,
+                self.getName() + ": Minerals - " + self.minerals()
+                        + ", Gas - " + self.gas() + ".");
+        game.drawTextScreen(30, 30, "Java\nTrainee!");
+    }
+
+
+    public void onUnitComplete(Unit unit) {
+        if (unit.getType().isWorker()) {
+            Unit closestMineral = null;
+            int closestDistance = Integer.MAX_VALUE;
+            for (Unit mineral : game.getMinerals()) {
+                int distance = unit.getDistance(mineral);
+                if (distance < closestDistance) {
+                    closestMineral = mineral;
+                    closestDistance = distance;
+                }
+            }
+            // Gather the closest mineral
+            unit.gather(closestMineral);
+        }
+    }
+
+    void run() {
+        bwClient = new BWClient(this);
+        bwClient.startGame();
     }
 
     public static void main(String[] args) {
-        Aqulisk bot = new Aqulisk();
-        bot.bwClient = new BWClient(bot);
-        bot.bwClient.startGame();
+        new Aqulisk().run();
     }
 }
